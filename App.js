2674623165c69fe29e3483ac64c7d72c916fd1e4
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-08-27 18:06:23 -0300

    feat: :sparkles: Entendo Props - Exibindo image e definindo variaveis

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 class App extends Component{
   render() {
+    let nome = 'Dhionathan Jobim';
+    let img = 'https://sujeitoprogramador.com/steve.png'
     return (
       <View>
         <Text>Hello Word</Text>
         <Text>Meu primeiro APP</Text>
+        <Text style={{ color: 'red', fontSize: 25 , margin: 15}}>
+          Sujeito Programador
+        </Text>
+        <Image source={{uri: img}} style={{width: 300, height: 300}}></Image>
+        <Text style={{ fontSize: 30 }}>{ nome }</Text>
       </View>
     );
   }
