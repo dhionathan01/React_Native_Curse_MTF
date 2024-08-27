af8ff3abb0f1696809212452bbe3005bcf5053a2
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-08-27 17:56:51 -0300

    feat: :sparkles: Criando primeiro componenten com estancia de class

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component{
+  render() {
+    return (
+      <View>
+        <Text>Hello Word</Text>
+        <Text>Meu primeiro APP</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
