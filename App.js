08619b18a046b87638125fa9a6f68c5cda3cb67a
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-08-27 18:10:32 -0300

    feat: :sparkles: Criando um component e chamando no component principal

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { View, Text, Image } from 'react-native';
 class App extends Component{
   render() {
     let nome = 'Dhionathan Jobim';
-    let img = 'https://sujeitoprogramador.com/steve.png'
     return (
       <View>
         <Text>Hello Word</Text>
@@ -12,11 +11,22 @@ class App extends Component{
         <Text style={{ color: 'red', fontSize: 25 , margin: 15}}>
           Sujeito Programador
         </Text>
-        <Image source={{uri: img}} style={{width: 300, height: 300}}></Image>
-        <Text style={{ fontSize: 30 }}>{ nome }</Text>
+        <Text style={{ fontSize: 30 }}>{nome}</Text>
+        <Jobs></Jobs>
       </View>
     );
   }
 }
-
 export default App;
+
+class Jobs extends Component {
+  render() { 
+    let img = 'https://sujeitoprogramador.com/steve.png'
+    return (
+      <Image
+        source={{ uri: img }}
+        style={{ width: 300, height: 300 }}
+      ></Image>
+    );
+  }
+}
