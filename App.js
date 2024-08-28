640048e610c7fb91a23d3a855515273e1dca4450
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-08-28 10:59:37 -0300

    feat: :sparkles: Tamanhos Dinamicos e fixos

--- App.js
+++ App.js
@@ -4,8 +4,122 @@ import { View, Text, StyleSheet, Image, Button } from 'react-native';
 class App extends Component{
   render() {
     return (
-      <View>
-        <View></View>
+      <View style={{ flex: 1 , backgroundColor: '#555'}}>
+        <View style={{ flex: 2, backgroundColor: 'red' }}>
+          {/* Não importa quantos texto tenha, o conteúdo só vai ser exibido até o a quantidade que a view pode ocupar de tamanho */}
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+          <Text>Meu primeiro texto aqui</Text>
+        </View>
+        <View style={{flex:1,  backgroundColor: 'green' }}></View>
+        <View style={{height: 65,  backgroundColor: 'orange' }}></View>
+        <View style={{flex:3,  backgroundColor: 'yellow' }}></View>
       </View>
     );
   }
--- previews_comit/readme.md
+++ previews_comit/readme.md
@@ -3,3 +3,5 @@
 
 * Aplicando grupo de estilos:
 * ![alt text](image-1.png)
+* Tamanhos fixos e dinâmicos:
+* ![alt text](image-2.png)
