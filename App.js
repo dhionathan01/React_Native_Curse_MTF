ae92cbb0cf0172b0c7d9a9c2d4e9a83a36e0f77c
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-08-28 10:34:07 -0300

    refactor: :fire: Limpando projeto

--- App.js
+++ App.js
@@ -2,25 +2,10 @@ import React, {Component} from 'react';
 import { View, Text, Image, Button } from 'react-native';
 
 class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      nome: 'Dhionathan Jobim'
-    }
-    this.entrar = this.entrar.bind(this);
-  }
-  entrar(nome){
-    this.setState({
-      nome: nome
-    })
-  }
   render() {
     return (
       <View style={{ marginTop: 20 }}>
-        <Button title="Entrar" onPress={() => this.entrar('Dhionathan React Call')}></Button>
-        <Text style={{ fontSize: 23, color: 'red', textAlign: 'center' }}>
-          {this.state.nome}
-        </Text>
+     
       </View>
     );
   }
