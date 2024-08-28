b06ed0d9237bc2fc12890f54d41b3dc37dd773fe
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-08-28 11:28:20 -0300

    refactor: :fire: Limpando projeto

--- App.js
+++ App.js
@@ -4,18 +4,8 @@ import { View, Text, StyleSheet, Image, Button } from 'react-native';
 class App extends Component{
   render() {
     return (
-      /* flexDirection: row */
-      /* alignItems : Define na vertical .  altura  eixo y*/
-      /* justifyContent : Define na horizontal .  largura eixo x */
-      /* flexDirection: column */
-      /* alignItems : Define na horizontal .  largura  eixo x */
-      /* justifyContent : Define na vertical .  altura eixo y */
+      <View >
 
-
-      <View style={{ flex: 1 , flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-end', backgroundColor: '#555'}}>
-        <View style={{width: 50,  height: 50, backgroundColor: 'green' }}></View>
-        <View style={{width: 50,  height: 50, backgroundColor: 'red' }}></View>
-        <View style={{width: 50,  height: 50, backgroundColor: 'yellow' }}></View>
       </View>
     );
   }
