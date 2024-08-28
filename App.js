ba3d9d9d92e555474a409b61afe7a33c7d17b3e7
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-08-28 10:47:59 -0300

    refactor: :fire: Limpando projeto

--- App.js
+++ App.js
@@ -4,25 +4,10 @@ import { View, Text, StyleSheet, Image, Button } from 'react-native';
 class App extends Component{
   render() {
     return (
-      <View style={styles.area}>
-        <Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Eu sou texto 1</Text>
-        <Text style={styles.alinhaTexto}>Eu sou texto 2</Text>
-        <Text>Eu sou texto 3</Text>
-        <Text style={styles.textoPrincipal}>Eu sou texto 4</Text>
+      <View>
+        <View></View>
       </View>
     );
   }
 }
-const styles = StyleSheet.create({
-  area: {
-    marginTop: 20
-  },
-  textoPrincipal: {
-    fontSize: 25,
-    color: '#FF0000'
-  },
-  alinhaTexto: {
-    textAlign: 'center'
-  }
-});
 export default App;
